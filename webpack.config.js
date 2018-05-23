5dd6c76c8fa2c5ff402f02c6022d19d9e22372d7
ralph salazar <ralph.slzr@gmail.com>
2018-05-22 23:15:14 -0400

    bio image served

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './client/index.js',
   output: {
-    path: path.resolve(__dirname, '/build'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
   },
   module: {
@@ -20,8 +20,25 @@ module.exports = {
         test: /\.css$/,
         exclude: /node_modules/,
         loaders: ['style-loader', 'css-loader']
-      }
-    ]
+      },
+      {
+        test: /\.(jpg|png|gif|svg|pdf|ico)$/,
+          use: [
+              {
+                loader: 'file-loader',
+                options: {
+                name: '[path][name]-[hash:8].[ext]'
+                   },
+          }]
+       },
+      {
+      test: /\.(pdf|jpg|png|gif|svg|ico)$/,
+      use: [
+          {
+            loader: 'url-loader'
+          }
+        ]
+    }]
   },
   resolve: {
     extensions: ['.js', '.jsx', '.css'],
